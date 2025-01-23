55b5f65f60730ce2dcf9cbcf86acb95c8c751f8a
takuya-sangil <jayloveswonyoung@gmail.com>
2025-01-23 22:57:15 +0800

    updated file for manual push

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 # backend.tf
 terraform {
-    backend "s3" {
-        bucket = "ruka-terraform-state"
-        key = "terraform/state"
-        region = "us-east-1"
-        dynamodb_talbe = "terraform-lock"
-    }
+  backend "s3" {
+    bucket         = "ruka-terraform-state"
+    key            = "terraform/state"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
 }
